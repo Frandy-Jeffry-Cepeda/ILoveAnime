ec8bec2fc917d22e2f93ec386f7aa7adc55efdca
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-05 11:29:04 -0400

    Update animeSplice.ts

--- IloveAnime/src/store/animeSplice.ts
+++ IloveAnime/src/store/animeSplice.ts
@@ -1,20 +1,31 @@
 import { StateCreator } from "zustand"
-import { getAnimeSeasonNow } from "../services/AnimeService"
-import { animeSeasonNow } from "../types"
+import { getAnimeRecentEpisodes, getAnimeSeasonNow } from "../services/AnimeService"
+import { animeRecentEpisodes, animeSeasonNow } from "../types"
 
 export type AnimeSliceType = {
     animeSeasonNow: animeSeasonNow
+    animeRecentEpisodes: animeRecentEpisodes
     getAnimeSeason: () => Promise<void>
+    getAnimeRecentEpisodes: () => Promise<void>
 }
 
 export const createAnimeSlice: StateCreator<AnimeSliceType> = (set) => ({
     animeSeasonNow: {
         data: []
     },
+    animeRecentEpisodes: {
+        data: []
+    },
     getAnimeSeason: async () => {
         const animeSeasonNow = await getAnimeSeasonNow()
         set({
           animeSeasonNow
+        })
+    },
+    getAnimeRecentEpisodes: async () => {
+        const animeRecentEpisodes = await getAnimeRecentEpisodes()
+        set({
+            animeRecentEpisodes
         })
     }
 })
