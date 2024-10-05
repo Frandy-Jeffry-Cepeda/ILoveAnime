4fb3a04e661e2423c34c92548f1860efac876f38
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-05 11:28:53 -0400

    Update IndexPage.tsx

--- IloveAnime/src/views/IndexPage.tsx
+++ IloveAnime/src/views/IndexPage.tsx
@@ -2,19 +2,22 @@ import { useEffect } from "react";
 import { useAppStore } from "../store/useAppStores";
 import MainMenu from "../components/MainMenu";
 import Aside from "../components/Aside";
+import Main from "../components/Main";
 
 export default function IndexPage() {
 
   const getAnimeSeasonNow = useAppStore((state) => state.getAnimeSeason);
+  const getAnimeRecentEpisodes = useAppStore((state) => state.getAnimeRecentEpisodes);
 
   useEffect(() => {
     getAnimeSeasonNow();
+    getAnimeRecentEpisodes();
   }, []);
 
   return (
     <>
-      <div className="container">
-        <div className="w-5/6 bg-black text-white py-5 my-5 text-center mx-auto rounded-lg border-b-[5px] border-b-indigo-400">
+      <div className="w-5/6 mx-auto">
+        <div className="bg-black text-white py-5 my-5 text-center  rounded-lg border-b-[5px] border-b-indigo-400">
           <h2 className="text-xl"><span className="font-bold text-2xl">ILoveAnime</span> tu biblioteca de Animes Favorita.</h2>
         </div>
 
@@ -22,29 +25,16 @@ export default function IndexPage() {
             <MainMenu/>
         </div>
 
-        <div className="flex flex-col md:flex-row w-5/6 mx-auto">
+        <div className="flex flex-col md:flex-row">
           
-        <div className="bg-white shadow w-full md:w-1/4 h-auto overflow-y-auto mr-5 mb-5 md:mb-0">
-          <Aside/>
-        </div>
-
-          <div className="bg-black w-full md:w-3/4 max-h-screen overflow-y-auto">
-            <main className="flex flex-col text-white h-full">
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-              <p>asd</p>
-            </main>
-            
+          <div className="bg-white shadow w-full md:w-1/4 h-1/4 overflow-y-auto mr-5 mb-5 md:mb-0">
+            <Aside/>
           </div>
+
+          <div className="md:w-3/4 overflow-y-auto">
+            <Main/>
+          </div>
+
         </div>
       </div>
     </>
