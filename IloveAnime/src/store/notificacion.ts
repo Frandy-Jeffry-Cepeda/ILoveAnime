ea3ec24491100f1bd0a3a4732a898d8092a3a6d0
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-06 19:33:55 -0400

    Create notificacion.ts

---
+++ IloveAnime/src/store/notificacion.ts
@@ -0,0 +1,42 @@
+import { StateCreator } from 'zustand'
+import { AnimeFavoriteSliceType } from './animeFavoriteSlice'
+
+type Notification = {
+    text: string
+    error: boolean
+    show: boolean
+}
+
+export type NotificationSliceType = {
+    notification: Notification
+    showNotification: (payload: Pick<Notification, 'text' | 'error'>) => void
+    hideNotification: () => void
+
+}
+
+export const createNotificationlice: StateCreator<NotificationSliceType & AnimeFavoriteSliceType, [], [], NotificationSliceType> = (set, get) => ({
+  notification: {
+    text: '',
+    error: false,
+    show: false
+  },
+  showNotification: (payload) => {
+      set({
+        notification: {
+          text: payload.text,
+          error: payload.error,
+          show: true
+        }
+      })
+      setTimeout(() => {get().hideNotification()}, 5000)
+  },
+  hideNotification: () => {
+    set({
+      notification: {
+        text: '',
+        error: false,
+        show: false
+      }
+    })
+  }
+})
