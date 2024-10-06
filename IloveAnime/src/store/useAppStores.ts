fade49904bc95d06feb14a55f0d42626a0b0c195
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-06 19:33:51 -0400

    Update useAppStores.ts

--- IloveAnime/src/store/useAppStores.ts
+++ IloveAnime/src/store/useAppStores.ts
@@ -1,7 +1,11 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware"; 
-import { AnimeSliceType, createAnimeSlice } from "./animeSplice";
+import { AnimeSliceType, createAnimeSlice } from "./animeSlice";
+import { AnimeFavoriteSliceType, createAnimeFavoriteSlice } from "./animeFavoriteSlice";
+import { createNotificationlice, NotificationSliceType } from "./notificacion";
 
-export const useAppStore = create<AnimeSliceType>()(devtools((set, get, ...a) => ({
-    ...createAnimeSlice(set, get, ...a), 
+export const useAppStore = create<AnimeSliceType & AnimeFavoriteSliceType & NotificationSliceType>()(devtools((...a) => ({
+    ...createAnimeSlice(...a), 
+    ...createAnimeFavoriteSlice(...a),
+    ...createNotificationlice(...a)
 })))
