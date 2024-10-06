219af217bf35a6a401586866b0c1d89262a85453
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-05 20:35:07 -0400

    Update Main.tsx

--- IloveAnime/src/components/Main.tsx
+++ IloveAnime/src/components/Main.tsx
@@ -2,25 +2,25 @@ import { useAppStore } from "../store/useAppStores";
 
 export default function Main() {
     const animeRecentEpisodes = useAppStore((state) => state.animeRecentEpisodes);
+    const setSelectedAnimeEpisode = useAppStore((state) => state.setSelectedAnimeEpisode);
 
     return (
         <>
             <div className="p-4">
-
                 <div className="mb-4 text-center md:text-start">
                     <h2 className="text-2xl font-bold mb-5">Últimos capítulos</h2>
                 </div>
-
 
                 <main className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
                     {animeRecentEpisodes.data.map(anime => (
                         <div 
                             key={anime.entry.mal_id} 
-                            className="relative rounded-lg transform transition hover:scale-105"
-                            style={{ height: "300px" }} // Ajuste del tamaño aquí
+                            className="relative rounded-lg transform transition hover:scale-105 cursor-pointer"
+                            style={{ height: "300px" }}
+                            onClick={() => setSelectedAnimeEpisode(anime.entry.mal_id)} // Establecer el episodio seleccionado
                         >
                             <img 
-                                src={anime.entry.images.webp.image_url} 
+                                src={anime.entry.images.jpg.image_url} 
                                 alt={anime.entry.title} 
                                 className="w-full h-full object-cover rounded-lg"
                             />
