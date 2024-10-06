aa0b5e9ca0af43bc316969ad69d17b4c778f8cf8
Frandy Jeffry Cepeda <frandycepeda1906@gmail.com>
2024-10-06 19:34:10 -0400

    Update Layout.tsx

--- IloveAnime/src/layout/Layout.tsx
+++ IloveAnime/src/layout/Layout.tsx
@@ -1,10 +1,23 @@
 import { Outlet } from "react-router-dom"
 import Header from "../components/Header"
 import Modal from "../components/Modal"
+import { useAppStore } from "../store/useAppStores"
+import { useEffect } from "react"
+import Notification from "../components/Notificacion"
+import Aside from "../components/Aside"
 
 export default function Layout() {
+
+  const loadFromStorage = useAppStore((state) => state.loadFromStorage)
+
+  useEffect(() => {
+    loadFromStorage()
+  } ,[])
+
   return (
     <>
+
+    
     <Header/>
     
     <main>
@@ -12,6 +25,7 @@ export default function Layout() {
     </main>
 
     <Modal/>
+    <Notification/>
 
     
     </>
